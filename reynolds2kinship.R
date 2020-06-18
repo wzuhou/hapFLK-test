a2eefec7cf977a5b479763616b961630a25f7b13
wzuhou <wuz1992@hotmail.com>
2020-06-18 16:20:11 +0200

    Update reynolds2kinship.R

--- reynolds2kinship.R
+++ reynolds2kinship.R
@@ -1,42 +1,45 @@
 library(ape)
 library(phangorn)
 args <- commandArgs(T)
-midpoint= function(tree){
-    require(phangorn)
-    dm = cophenetic(tree)
-    tree = unroot(tree)
-    rn = max(tree$edge)+1
-    maxdm = max(dm)
-    ind =  which(dm==maxdm,arr=TRUE)[1,]
-    tmproot = Ancestors(tree, ind[1], "parent")
-    tree = phangorn:::reroot(tree, tmproot)
-    edge = tree$edge
-    el = tree$edge.length
-    children = tree$edge[,2]
-    left = match(ind[1], children)
-    tmp = Ancestors(tree, ind[2], "all")
-    tmp= c(ind[2], tmp[-length(tmp)])
-    right = match(tmp, children)
-    if(el[left]>= (maxdm/2)){
-         edge = rbind(edge, c(rn, ind[1]))
-         edge[left,2] = rn
-         el[left] = el[left] - (maxdm/2)
-         el = c(el, maxdm/2)
-    }
-    else{
-        sel = cumsum(el[right])
-        i = which(sel>(maxdm/2))[1]
-        edge = rbind(edge, c(rn, tmp[i]))
-        edge[right[i],2] = rn
-        eltmp =  sel[i] - (maxdm/2)
-        el = c(el, el[right[i]] - eltmp)
-        el[right[i]] = eltmp
-    }
-    tree$edge.length = el
-    tree$edge=edge
-    tree$Nnode  = tree$Nnode+1
-    phangorn:::reorderPruning(phangorn:::reroot(tree, rn))
-}
+###phangorn:::reorderPruning was not supported
+# midpoint= function(tree){
+#     require(phangorn)
+#     dm = cophenetic(tree)
+#     tree = unroot(tree)
+#     rn = max(tree$edge)+1
+#     maxdm = max(dm)
+#     ind =  which(dm==maxdm,arr=TRUE)[1,]
+#     tmproot = Ancestors(tree, ind[1], "parent")
+#     tree = phangorn:::reroot(tree, tmproot)
+#     edge = tree$edge
+#     el = tree$edge.length
+#     children = tree$edge[,2]
+#     left = match(ind[1], children)
+#     tmp = Ancestors(tree, ind[2], "all")
+#     tmp= c(ind[2], tmp[-length(tmp)])
+#     right = match(tmp, children)
+#     if(el[left]>= (maxdm/2)){
+#          edge = rbind(edge, c(rn, ind[1]))
+#          edge[left,2] = rn
+#          el[left] = el[left] - (maxdm/2)
+#          el = c(el, maxdm/2)
+#     }
+#     else{
+#         sel = cumsum(el[right])
+#         i = which(sel>(maxdm/2))[1]
+#         edge = rbind(edge, c(rn, tmp[i]))
+#         edge[right[i],2] = rn
+#         eltmp =  sel[i] - (maxdm/2)
+#         el = c(el, el[right[i]] - eltmp)
+#         el[right[i]] = eltmp
+#     }
+#     tree$edge.length = el
+#     tree$edge=edge
+#     tree$Nnode  = tree$Nnode+1
+#     phangorn:::reorderPruning(phangorn:::reroot(tree, rn))
+# }
+
+
 
 kinship=function(D,outgroup="",keep_outgroup=FALSE)
   {
@@ -45,6 +48,7 @@ kinship=function(D,outgroup="",keep_outgroup=FALSE)
     npop=sqrt(length(D))
     PopTree=nj(D)
     if (outgroup=="") {
+      require(phangorn)
       PopTree=midpoint(PopTree)
       npop=npop+1
     } else {
